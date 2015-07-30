3ee62d1db1e2d967c17077cf263c75f412211cf8
user6572726f72 <user6572726f72@gmail.com>
2015-07-30 00:15:49 -0700

    g1

--- www/src/Game.js
+++ www/src/Game.js
@@ -52,7 +52,7 @@ BasicGame.Game.prototype = {
 
         // Here we load the assets required for our preloader (in this case a 
         // background and a loading bar)
-        this.load.image('logo', 'asset/phaser20.png');
+        this.load.image('logo', 'asset/phaser21.png');
     },
 
     create: function () {
