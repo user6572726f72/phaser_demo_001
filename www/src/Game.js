0982ec325cfb273e8186709c7e109804b907248a
user6572726f72 <user6572726f72@gmail.com>
2015-07-29 21:49:19 -0700

    qcommit

--- www/src/Game.js
+++ www/src/Game.js
@@ -52,7 +52,7 @@ BasicGame.Game.prototype = {
 
         // Here we load the assets required for our preloader (in this case a 
         // background and a loading bar)
-        this.load.image('logo', 'asset/phaser.png');
+        this.load.image('logo', 'asset/phaser2.png');
     },
 
     create: function () {
